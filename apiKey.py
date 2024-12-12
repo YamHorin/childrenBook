d890592fd0c2a8df0d9ab7d26849853fdf12130b
Yam Horin <horinyam@gmail.com>
2024-12-12 15:32:28 +0200

    12.12

--- apiKey.py
+++
@@ -1,3 +0,0 @@
-apikey ='''
-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
-'''
--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from openai import OpenAI
-from PIL import Image
-import streamlit as st
-#get api key
-from apiKey import apikey
+import torch
+from torchvision.utils import save_image
+from diffusers import StableDiffusionPipeline
+from ollama import chat
+from ollama import ChatResponse
 
+# Load the pre-trained Stable Diffusion model
+def makeImages(prompt):
+    model_id = "CompVis/stable-diffusion-v1-4"
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe = pipe.to("cuda")  # Use GPU for faster processing
 
-st.set_page_config(page_title="demo children book app" , page_icon=":camera:" , layout="wide")
-st.title("demo")
+    # Generate an image
+    image = pipe(prompt).images[0]
 
-input_text = st.text_input("enter description for the book")
-num_of_images = st.number_input("select the number of images you want in the book" , min_value=1 , max_value=
-                                10 , value=1)
+    # Save the image
+    image.save("output.png")
+    print("Image saved as output.png")
+    
+def makeTextAi(prompt):
+    response: ChatResponse = chat(model='llama3.2', messages=[
+    {
+        'role': 'user',
+        'content': prompt,
+    },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
 
-if (st.button("Genrate")):
-    pass
-
+makeTextAi("make a story time for children not longer then 10 lines")
